f309eb023f6f04370bf6b8060ae2bcc63b961f95
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2021-07-22 11:52:32 +0530

    ex: 04
    learn callback

--- program.js
+++ program.js
@@ -1,7 +1,8 @@
 var buttonTranslate = document.querySelector("#button-translate")
-
+var userInput = document.querySelector("#textarea")
 function callback(){
     console.log("CLicked")
+    console.log("Input" , userInput.value)
 }
 
 var buttonclick = buttonTranslate.addEventListener("click", callback)
